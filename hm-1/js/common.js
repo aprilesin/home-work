ee85deb9c303692e592d53aa162dd6373db1b481
Vadim Lesin <aprilesin@gmail.com>
2017-10-22 01:31:54 +0300

    home work 1 added changes

--- hm-1/js/common.js
+++ hm-1/js/common.js
@@ -1,19 +1,24 @@
 var dog = {
 	color: 'black',
-	lenght: 1,
+	length: 1,
 	weight: 10,
 	paws: 4,
 	jaws:{
 		teeth: 42
 	},
 	getVoice: function(){
-		return 'gavgav';
+		console.log('gavgav')
+		return cat;
 	}
 };
+var cat = {
+	color: 'ginger',
+	length:  0.5
+};
 
-dog.lenght = 2;
+dog.length = 2;
 console.log(dog);
-console.log(dog.lenght);
+console.log(dog.length);
 console.log(dog.getVoice());
 
 console.log(dog.jaws.teeth);
@@ -21,3 +26,8 @@ dog.jaws.teeth = 50;
 console.log(dog.jaws.teeth);
 console.log(dog.color.toUpperCase());
 console.log(typeof(dog.color));
+
+console.log(cat.color);
+console.log(cat.length);
+
+console.log(dog.getVoice());
