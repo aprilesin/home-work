2545054de90dff753dda9163a7d97e063a44a4a8
Vadim Lesin <aprilesin@gmail.com>
2017-11-04 00:57:41 +0200

    shortened and removed unnecessary

--- hm-4/js/common.js
+++ hm-4/js/common.js
@@ -1,27 +1,23 @@
-var blocks = document.getElementsByClassName('block');
-console.log(blocks.length);
+var wrap = document.getElementById('wrapper');
+var blocks = wrap.getElementsByTagName("div");
 var turn = 0;
 var tmp = 0;
 
-document.getElementById('wrapper').onclick = function (event) {
-    if ((event.target.className === 'block') && (event.target.outerText !== 'X') && (event.target.outerText !== 'O')){
-            event.target.innerHTML = 'X';
-            setO(Math.floor(Math.random() * (blocks.length - 1)));
+wrap.onclick = function (event) {
+    if (!event.target.outerText){
+        event.target.innerHTML = 'X';
+        setO();
     }
 };
 
-function setO(a) {
-    for (a; a < blocks.length - 1; a ++){
-       if ((blocks[a].outerText !== 'X') && (blocks[a].outerText !== 'O')) {
-           blocks[a].innerHTML = 'O';
-           a = blocks.length - 1;
-           tmp = 1; 
-           turn++;
-       }
-      if ((a == blocks.length - 2) && (tmp == 0) && (turn < 4))
-       {
-          a = 0;
-       }
-      tmp=0;
-    }
+function setO() {
+    do {
+    a = Math.floor(Math.random() * (blocks.length - 1));
+    if (!blocks[a].outerText) {
+      blocks[a].innerHTML = 'O';
+      tmp = 1;
+      turn++;
+     }
+    } while (!tmp && (turn < 4))
+    tmp=0;
 }
